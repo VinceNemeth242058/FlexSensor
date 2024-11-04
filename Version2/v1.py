a79ad37402dc9b9e758090e5a19758be11bd448a
BenedekVass240814 <240814@buas.nl>
2024-11-04 14:54:39 +0100

    python file change

--- Version2/v1.py
+++ Version2/v1.py
@@ -1,7 +1,12 @@
 import serial
 
 ser = serial.Serial("COM5", baudrate=9600, timeout=1)
+sample_size = 10
+sample = [0 for i in range(sample_size)]
+current_index = 0
 while True:
    data = ser.readline()
-   print(data)
-   
+   data = data.strip().split()
+   sample[current_index] = data
+   current_index = current_index + 1 if current_index - 1 < sample_size else 0
+   print(sample)
