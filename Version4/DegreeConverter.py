5270af84c1dd8d32d99c631ee1f08233093ceed6
BenedekVass240814 <240814@buas.nl>
2024-11-05 13:21:25 +0100

    Create DegreeConverter.py

---
+++ Version4/DegreeConverter.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy.polynomial.polynomial import Polynomial
+
+
+class DegreeConverter:
+    def __init__(self):
+        angles = np.array([0, 30, 45, 60, 90])
+        measures = np.array([0, ])
+        self.p = Polynomial.fit(measures, angles)
+
+    def calculate(self, bit_value: int) -> int:
+        return self.p(bit_value)
+    
